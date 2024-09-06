065ac7f11e37c1e06ae4e71ff36ef1fce5bf64d5
Roger Albuquerque <117314615+DevRogerAlbuquerque@users.noreply.github.com>
2024-09-06 13:36:16 +0000

    Alterando div itemLoja para componente

--- src/components/Store/background.js
+++ src/components/Store/background.js
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useScroll } from "../../hooks/scroll";
+import ItemLoja from "../Membro/ItemLoja";
 
 export default function Background() {
   const backgroundRef = useRef(null);
@@ -11,27 +12,14 @@ export default function Background() {
       <div className="decks-container">
         <button className="scroll-button scroll-left" onClick={scrollLeft}>{"<"}</button>
         <div className="decks" id="planos" ref={backgroundRef}>
-          <div class = "itemLoja">
-            <h3>Deserto</h3>
-            <img src="images/natureza.png" alt="Deserto" />
-          </div>
-          <div class = "itemLoja">
-            <h3>Natureza</h3>
-            <img src="images/natureza2.png" alt="Natureza" />
-          </div>
-          <div class = "itemLoja">
-            <h3>Deserto2</h3>
-            <img src="images/deserto.png" alt="Deserto2" />
-          </div>
-          <div class = "itemLoja">
-            <h3>Deserto2</h3>
-            <img src="images/deserto.png" alt="Deserto2" />
-          </div>
-          <div class = "itemLoja">
-            <h3>Deserto2</h3>
-            <img src="images/deserto.png" alt="Deserto2" />
-          </div>
-            
+          {
+            [{name: 'Deserto', imgSrc: 'images/natureza.png'},
+              {name: 'Natureza', imgSrc: 'images/natureza2.png'},
+              {name: 'Deserto 2', imgSrc: 'images/deserto.png'},
+              {name: 'Deserto 2', imgSrc: 'images/deserto.png'},
+              {name: 'Deserto 2', imgSrc: 'images/deserto.png'},
+            ].map(item => <ItemLoja name={item.name} imgSrc={item.imgSrc}/>)
+          }
         </div>
         <button className="scroll-button scroll-right" onClick={scrollRight}>{">"}</button>
       </div>
--- src/components/Store/music.js
+++ src/components/Store/music.js
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useScroll } from "../../hooks/scroll";
+import ItemLoja from "../Membro/ItemLoja";
 
 
 export default function Music() {
@@ -12,31 +13,13 @@ export default function Music() {
       <div className="decks-container">
         <button className="scroll-button scroll-left" onClick={scrollLeft}>{"<"}</button>
         <div className="decks" id="planos" ref={musicRef}>
-            <div className="itemLoja" >
-                <h3>Música 1</h3>
-                <img src="images/audio_icon.png" alt="Ícone de Áudio" className="audio-icon" />
-                <audio src="audio/musica1.mp3" preload="none"></audio>
-            </div>
-
-            <div className="itemLoja" >
-                <h3>Música 2</h3>
-                <img src="images/audio_icon.png" alt="Ícone de Áudio" className="audio-icon" />
-                <audio src="audio/musica2.mp3" preload="none"></audio>
-            </div>
-
-            <div className="itemLoja" >
-                <h3>Música 3</h3>
-                <img src="images/audio_icon.png" alt="Ícone de Áudio" className="audio-icon" />
-                <audio src="audio/musica3.mp3" preload="none"></audio>
-            </div>
-
-            <div className="itemLoja" >
-                <h3>Música 4</h3>
-                <img src="images/audio_icon.png" alt="Ícone de Áudio" className="audio-icon" />
-                <audio src="audio/musica4.mp3" preload="none"></audio>
-            </div>
-
-
+            {[1, 2, 3, 4].map(index => 
+                <ItemLoja name={`Música ${index}`} 
+                  imgSrc="images/audio_icon.png" 
+                  audioSrc={`audio/musica${index}.mp3`}>
+                    <audio src={`audio/musica${index}.mp3`} preload="none"></audio>
+                  </ItemLoja>
+              )}
         </div>
         <button className="scroll-button scroll-right" onClick={scrollRight}>{">"}</button>
       </div>
