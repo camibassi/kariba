e2993b87ff8ea82c4f61f3ac64c963953a4941d1
Roger Albuquerque <117314615+DevRogerAlbuquerque@users.noreply.github.com>
2024-09-09 13:00:10 +0000

    Excluindo comentários

--- src/hooks/UseCartasMao.js
+++ src/hooks/UseCartasMao.js
@@ -1,13 +1,5 @@
 import { useState } from "react";
 
-/*
-UseCartasMao obj = new UseCartasMao()
-obj.cartas = [];
-obj.adicionarCarta(){ 
-   // Adiciona uma carta
-}
-obj.removeCarta() {}
-*/
 const UseCartasMao = function() 
 {
     const [cartas, setCartas] = useState([]);
@@ -41,49 +33,3 @@ const UseCartasMao = function()
 }
 
 export default UseCartasMao;
-/*
-import React, { useState } from 'react';
-
-
-function cartasMao() {
-  const [cartas, setCartas] = useState(["1", "3", "4", "5", "6"]);
-  
-  const [input, setInput] = useState('');
-
-  function handleAdd(){
-    setCartas([...cartas, input])
-  }
-
-
-
-  return (
-    <div>
-
-      <ul>
-        {cartas.map(tarefa => (
-          <li key={tarefa}>{tarefa}</li>
-        ))}
-      </ul>
-
-      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>    
-      <button type="button" onClick={handleAdd}>Adicionar</button>
-
-    </div>
-  );
-}
-
-export default cartasMao;
-
-import React, { useState } from 'react';
-
-
-function CartasMao() {
-  const [number, setNumber] = useState(4);
-  function change()
-  {
-    setNumber( Math.round(Math.random()) );
-    alert(number);
-  }
-}
-
-export default CartasMao; */
