91f16785248b2517292c02c757dd2db98f980ea1
GRBrito <brito.g.rodrigues@gmail.com>
2024-10-07 08:48:23 -0300

    algumas alterações nos titulos, alinhamento e opacidade em regras.

--- src/routes/regras/index.js
+++ src/routes/regras/index.js
@@ -13,62 +13,48 @@ export default function Regras()
         </MenuNavbar>
         <Container fluid>
             <section>
-                <h2 id="regras">Objetivo do Jogo</h2>
+                <img src="images/objetivo.png"></img>
                 <p>O objetivo de <strong>Kariba</strong> é capturar o maior número de cartas de animais. Os jogadores colocam animais próximos ao lago e usam a força de uns para afugentar os outros.</p>
-                <img className="divisor" src="images/divisor.png"></img>
             </section>
 
             <section>
-                <h2 id="regras">Preparação</h2>
+                <img src="images/preparacao.png"></img>
                 <ul>
                     <li>5 cartas são distribuidas para cada jogador.</li>
                     <li>O restante das cartas forma o monte de compras.</li>
                     <li>O tabuleiro circular com divisões numéricas de 1 a 8 é colocado no centro da mesa.</li>
                 </ul>
-                <img className="divisor" src="images/divisor.png"></img>
             </section>
             
             <section>
-                <h2 id="regras">Como Jogar</h2>
+                <img src="images/comoJogar.png"></img>
                 <ol>
                     <li>No seu turno, o jogador deve colocar uma ou mais cartas do mesmo tipo de animal em sua respectiva posição numérica no tabuleiro.</li>
                     <li>Se, após a jogada, houver 3 ou mais cartas do mesmo animal em um número, ele afugenta os animais numericamente mais fracos (adjacentes). Exemplo: 3 ou mais elefantes (8) afugentam os rinocerontes (7).</li>
                     <li>Os animais afugentados são capturados pelo jogador que fez a jogada e guardados como pontos.</li>
                     <li>Após a jogada, o jogador compra cartas do monte até completar 5 cartas em sua mão.</li>
                 </ol>
-                <img className="divisor" src="/images/divisor.png"></img>
             </section>
 
             <section>
-                <h2 id="regras">Animais e seus Números</h2>
+                <img src="images/cartas.png"></img>
                 <p>Cada animal tem um número que representa sua posição no tabuleiro:</p>
-                    <li>1 - Rato</li>
-                    <li>2 - Suricato</li>
-                    <li>3 - zebra</li>
-                    <li>4 - Girafa</li>
-                    <li>5 - Avestruz</li>
-                    <li>6 - Leopardo</li>
-                    <li>7 - Rinoceronte</li>
-                    <li>8 - Elefante</li>
-                    <img className="divisor" src="images/divisor.png"></img>
+                <img id="cartas" src="images/cartaskariba.png"></img>  
             </section>
 
             <section>
                 <h2 id="regras">Exceção</h2>
                 <p>Os ratos (número 1) são os únicos que podem afugentar os elefantes (número 8). Para isso, é necessário ter 3 ou mais ratos no tabuleiro.</p>
-                <img className="divisor" src="images/divisor.png"></img>
             </section>
 
             <section>
                 <h2 id="regras">Fim do Jogo</h2>
                 <p>O jogo termina quando todas as cartas do baralho foram jogadas e não restam mais cartas na mão dos jogadores.</p>
-                <img className="divisor" src="images/divisor.png"></img>
             </section>
 
             <section>
                 <h2 id="regras">Vencedor</h2>
                 <p>O vencedor é o jogador que capturou o maior número de cartas ao final do jogo.</p>
-                <img className="divisor" src="images/divisor.png"></img>
             </section>
 
             <footer>
