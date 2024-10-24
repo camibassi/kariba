bd62e508e8621e22e2be4a470979085e5695b9bc
GRBrito <brito.g.rodrigues@gmail.com>
2024-10-23 23:08:23 -0300

    tela do vencedor, para visualizar criei um botao de teste no menu principal

--- src/routes/vencedor/vencedor.js
+++ src/routes/vencedor/vencedor.js
@@ -1,19 +1,26 @@
-import './index.css'
+import React, { useEffect } from 'react';
+import './stylevencedor.css'
 import { Link } from "react-router-dom";
 import { Container } from 'react-bootstrap';
 
 export default function Vencedor()
 {
+  useEffect(() => {
+    const audio = new Audio('/sounds/somalegre.mp3'); // Caminho relativo da pasta public
+    audio.play(); // Toca o som ao carregar
+  }, []);
   return(
-    <div>
+    <div id="vencedor">
         <Container fluid>
-        <h1>TEMOS UM VENCEDOR</h1>
-        <p>EM BREVE TEREMOS UM NOVO REI NA SAVANA!</p>
+        <h1 id="vencedor">TEMOS UM</h1>
+        <h1 id="vencedor">VENCEDOR</h1>
+        <p id="vencedor">EM BREVE TEREMOS UM NOVO REI NA SAVANA!</p>
 
         <div class="button-container">
             <a href="#" class="button">JOGAR NOVAMENTE</a>
-            <a href="#" class="button">MENU PRINCIPAL</a>
+            <a href="menuPrincipal/index.js" class="button">MENU PRINCIPAL</a>
         </div>
+
         </Container>
         
     </div>
