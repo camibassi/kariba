2fdbf036b5ad667f7d319825f62736d9cc4d5d4c
GRBrito <brito.g.rodrigues@gmail.com>
2024-10-24 08:32:23 -0300

    finalizado tela do vencedor

--- src/routes/vencedor/vencedor.js
+++ src/routes/vencedor/vencedor.js
@@ -1,28 +1,29 @@
 import React, { useEffect } from 'react';
-import './stylevencedor.css'
+import './stylevencedor.css';
 import { Link } from "react-router-dom";
 import { Container } from 'react-bootstrap';
 
-export default function Vencedor()
-{
+export default function Vencedor() {
   useEffect(() => {
-    const audio = new Audio('/sounds/somalegre.mp3'); // Caminho relativo da pasta public
-    audio.play(); // Toca o som ao carregar
+    const audio = new Audio('/sounds/somalegre.mp3');
+    audio.play().catch(error => console.log("Erro ao reproduzir áudio:", error));
   }, []);
-  return(
-    <div id="vencedor">
+
+  return (
+    <div>
+      <div id="confetti"></div> {/* Confete fora de #vencedor */}
+      <div id="vencedor">
         <Container fluid>
-        <h1 id="vencedor">TEMOS UM</h1>
-        <h1 id="vencedor">VENCEDOR</h1>
-        <p id="vencedor">EM BREVE TEREMOS UM NOVO REI NA SAVANA!</p>
+          <h1 id="vencedor">TEMOS UM</h1>
+          <h1 id="vencedor">VENCEDOR</h1>
+          <p id="vencedor">EM BREVE TEREMOS UM NOVO REI NA SAVANA!</p>
 
-        <div class="button-container">
-            <a href="#" class="button">JOGAR NOVAMENTE</a>
-            <a href="menuPrincipal/index.js" class="button">MENU PRINCIPAL</a>
-        </div>
-
+          <div className="button-container">
+            <Link className="botao" to="/aguardando">Jogar Novamente</Link>
+            <Link className="botao" to="/menu">Menu Principal</Link>
+          </div>
         </Container>
-        
+      </div>
     </div>
-  ); 
+  );
 }
