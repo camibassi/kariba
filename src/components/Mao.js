5c7aa531501973313b1641ed72b5c51a4b7cc636
Rose <rponzeto@gmail.com>
2024-11-04 18:23:14 -0300

    Validação da função dragStar do componente Mão para evitar objeto indefinido

--- src/components/Mao.js
+++ src/components/Mao.js
@@ -3,20 +3,43 @@ import Carta from './Carta';
 
 const Mao = (props) => 
 {
-    //essa função armazena o valor da carta arrastada da mão para a pilha e manda o valor para o componente pilha  
-    function dragStart (e) {
-        let cartaDiv = e.target.parentElement;
-        let imagePath = e.target.attributes["src"].value;
-        let parts = imagePath.split('/');
-        let imageDrag = parts[parts.length - 1]; // 'verso.png'
-        imageDrag = imageDrag.split('.')[0]; // 'verso' (sem o .png)
+   
+    //Funçao anterior dando erro objeto sem valor ou indefinido
+    // essa função armazena o valor da carta arrastada da mão para a pilha e manda o valor para o componente pilha  
+   // function dragStart (e) {
+     //   let cartaDiv = e.target.parentElement;
+       // let imagePath = e.target.attributes["src"].value;
+       // let parts = imagePath.split('/');
+       // let imageDrag = parts[parts.length - 1]; // 'verso.png'
+       // imageDrag = imageDrag.split('.')[0]; // 'verso' (sem o .png)
+                
+       // e.dataTransfer.setData("cartaValor", imageDrag );
+       // e.dataTransfer.setData("cartaDiv", cartaDiv.attributes['id'].value );
 
-        e.dataTransfer.setData("cartaValor", imageDrag );
-        e.dataTransfer.setData("cartaDiv", cartaDiv.attributes['id'].value );
+    //}
 
-
+     // essa função armazena o valor da carta arrastada da mão para a pilha e manda o valor para o componente pilha  
+    function dragStart(e) {
+        // Confirma se e.target e e.target.attributes["src"] estão definidos
+        if (e.target && e.target.attributes["src"]) {
+            let cartaDiv = e.target.parentElement;
+            let imagePath = e.target.attributes["src"].value;
+            let parts = imagePath.split('/');
+            let imageDrag = parts[parts.length - 1]; // 'verso.png'
+            imageDrag = imageDrag.split('.')[0]; // 'verso' (sem o .png)
+    
+            // Verifica se `cartaDiv` possui um ID e se `imageDrag` está definido
+            if (cartaDiv && cartaDiv.attributes['id'] && imageDrag) {
+                e.dataTransfer.setData("cartaValor", imageDrag);
+                e.dataTransfer.setData("cartaDiv", cartaDiv.attributes['id'].value);
+            } else {
+                console.error("Elemento ou atributos estão indefinidos.");
+            }
+        } else {
+            console.error("O atributo 'src' está ausente no elemento alvo.");
+        }
     }
-
+    
     return(
         <>
             <div id="mao">
--- src/routes/sobre/index.js
+++ src/routes/sobre/index.js
@@ -1,65 +1,64 @@
-import './index.css';
+import './index.css'
 import MenuNavbar from "../../components/MenuNavbar";
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export default function Sobre() {
-  return (
+export default function Sobre()
+{
+  return(
     <div id="sobre">
-      <MenuNavbar>
-        <div id="navSobre">    
-          <h1>
-            <img src="/images/favicon.png" alt="Logo" /> Equipe Kariba <img src="/images/favicon.png" alt="Logo" />
-          </h1>
-          <Link className="icone" to="/user">
-            <img src="images/icone50.png" alt="User Icon" />
-          </Link>
+        <MenuNavbar>
+        <div id="navSobre" >    
+            <h1>
+                <img src="/images/favicon.png" alt="Logo" /> Equipe Kariba <img src="/images/favicon.png" alt="Logo" />
+            </h1>
+            <Link className="icone" to="/user"><img src="images/icone50.png"/></Link>
         </div>
-      </MenuNavbar>
-      <div className="organograma">
-        <div className="ceo-container">
-          <div className="box ceo">
-            <p>Alan Soares</p>
-            <span>Gerente</span>
-          </div>
+        </MenuNavbar>
+        <div class="organograma">
+            <div class="ceo-container">
+                <div class="box ceo">
+                    <p>Alan Soares</p>
+                    <span>Gerente</span>
+                </div>
+            </div>
+            <div class="container">
+                <div class="col">
+                    <div class="box front">
+                        <p>FRONT-END</p>
+                    </div>
+                    <div class="sub-boxes">
+                        <div class="box analista">Camila Bassi</div>
+                        <div class="box analista">Gabriel Rodrigues</div>
+                        <div class="box analista">Roger Albuquerque</div>
+                        <div class="box analista">Rosemeire Ponzeto</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box back">
+                        <p>BACK-END</p>
+                    </div>
+                    <div class="sub-boxes">
+                        <div class="box analista">Alan Soares</div>
+                        <div class="box analista">Amanda Pires</div>
+                        <div class="box analista">Isaac Figueiredo</div>
+                        <div class="box analista">Roger Albuquerque</div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="box bd">
+                        <p>BANCO DE DADOS</p>
+                    </div>
+                    <div class="sub-boxes">
+                        <div class="box analista">Raphael Carvalhal</div>
+                        <div class="box analista">Raul Cezar</div>
+                        <div class="box analista">Roger Tioji</div>
+                        <div class="box analista">Thaiany Paina</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="container">
-          <div className="col">
-            <div className="box front">
-              <p>FRONT-END</p>
-            </div>
-            <div className="sub-boxes">
-              <div className="box analista">Camila Bassi</div>
-              <div className="box analista">Gabriel Rodrigues</div>
-              <div className="box analista">Roger Albuquerque</div>
-              <div className="box analista">Rosemeire Ramos</div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="box back">
-              <p>BACK-END</p>
-            </div>
-            <div className="sub-boxes">
-              <div className="box analista">Alan Soares</div>
-              <div className="box analista">Amanda Pires</div>
-              <div className="box analista">Isaac Figueiredo</div>
-              <div className="box analista">Roger Albuquerque</div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="box bd">
-              <p>BANCO DE DADOS</p>
-            </div>
-            <div className="sub-boxes">
-              <div className="box analista">Raphael Carvalhal</div>
-              <div className="box analista">Raul Cezar</div>
-              <div className="box analista">Roger Tioji</div>
-              <div className="box analista">Thaiany Paina</div>
-              <div className="box analista">Roger Albuquerque</div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  );
+
+  ); 
 }
