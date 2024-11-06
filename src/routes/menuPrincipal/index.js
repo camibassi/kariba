e09477d727b1d5970f2461a56876fe0cf0ba00d0
GRBrito <brito.g.rodrigues@gmail.com>
2024-11-06 01:22:55 -0300

    glow up no menu principal. e fundo padrão alterado.

--- src/routes/menuPrincipal/index.js
+++ src/routes/menuPrincipal/index.js
@@ -14,7 +14,6 @@ export default function MenuPrincipal() {
     const navigate = useNavigate();
     const containerRef = useRef(null);
 
-        
     // Função para realizar o logout e redirecionar para a página inicial
     const handleLogout = () => {
         logout();
@@ -24,15 +23,14 @@ export default function MenuPrincipal() {
     // Extrai o primeiro nome do usuário
     const primeiroNome = user.nome ? user.nome.split(" ")[0].toUpperCase() : user.username?.split(" ")[0].toUpperCase();
 
-
     return (
-        <div ref={containerRef}>
+        <div className="tela-jogo" ref={containerRef}> {/* Novo contêiner para o background */}
             <Navbar className="navbar-menu">
-                    <Container className="d-flex justify-content-between">
+                <Container className="d-flex justify-content-between">
                     <Nav.Link className="navbar-link" onClick={handleLogout}>
                         <FaSignOutAlt className="navbar-icon" /> Sair
                     </Nav.Link>
-                                
+                    
                     <div className="d-flex align-items-center">
                         <div className="nameuser">
                             {user.nome?.toUpperCase() || user.username?.toUpperCase()}
@@ -40,24 +38,31 @@ export default function MenuPrincipal() {
                         <Link className="icone" to="/user">
                             <img src="images/icone50.png" alt="Ícone do usuário" />
                         </Link>
-                                    
                     </div>
-                    </Container>
-                </Navbar>            
+                </Container>
+            </Navbar>      
+
+            {/* Contêiner para centralizar a imagem do logo */}
+            <div className="logo-container">
+                <img className="logonovo" src="images/novologo.png" alt="logo" />
+            </div>
+            
             <header className="header">
-                <h1 className="menuprincipal">KARIBA: VIRTUAL</h1>
                 <div className="headline">
-                    <div className="rounded-pill bg-secondary">
-                    <h2>BEM VINDO, {primeiroNome}!
+                    <div className="placa">
+                        <h2><br/>BEM VINDO, {primeiroNome}!
                             <br/>
-                        VOCÊ ACHOU O MELHOR 
-                        <br/> JOGO DE CARTAS</h2>
+                            VOCÊ ACHOU O MELHOR 
+                            <br/> JOGO DE CARTAS
+                        </h2>
+                        <p className="text-center texto-movimento-continuo">
+                            O objetivo em Kariba é coletar <br/>o maior número possível de cartas. <br/>Cada animal tem um número <br/>que corresponde <br/>ao seu poder na Savana.
+                        </p>
                     </div>
-                    <p className="text-center">O objetivo em Kariba é coletar o maior número possível de cartas. Cada animal tem um número que corresponde ao seu poder na Savana.</p>
                 </div>
+                
                 <div className="imgheadline">
                     <Link className="botao" onClick={() => {
-                        
                         webSocket.open();
                     }} to="/aguardando">Jogar</Link>
                     <Link className="botao" to="/store">Loja</Link>
